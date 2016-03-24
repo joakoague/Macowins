cd5a34b56d63b8dc0416d4c4b3d1de34abc052ff
Julian <julian@queteimporta.com>
2016-03-24 17:02:33 -0300

    Ya calcula total de ventas, y test agregado

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -9,12 +9,10 @@ import java.util.stream.Stream;
 
 public class Main {
 	public static double valorFijo = 100;
-	public static String input;
-	public static Scanner s = new Scanner(System.in);
-	public static ArrayList<Ventas> Arrayventas = new ArrayList<Ventas>();
-
+	private static String input;
+	private static Scanner s = new Scanner(System.in);
+	private static ArrayList<Ventas> Arrayventas = new ArrayList<Ventas>();
 	public static void main(String[] args) {
-
 		boolean flag1 = true;
 		int n1;
 		/*
@@ -26,18 +24,17 @@ public class Main {
 		while (flag1) {
 
 			System.out.println("Ingrese que operacion desea realizar: ");
-			System.out.println("1: calcular precio");
-			System.out.println("2: lista de ventas");
-			System.out.println("3: terminar");
+			System.out.println("1 = calcular precio");
+			System.out.println("2 = lista de ventas");
+			System.out.println("3 = salir");
 			input = s.nextLine();
 			n1 = Integer.parseInt(input);
-
 			switch (n1) {
 			case 1:
 				calcularPrecio();
 				break;
 			case 2:
-				mostrarVentas();
+				calcularVentas(Arrayventas);
 				break;
 				/*
 				 * System.out.println(""); Iterator it = Arrayventas.iterator();
@@ -56,17 +53,14 @@ public class Main {
 		int n2;
 		loop2: while (flag2) {
 			System.out.println("\nIngrese el tipo de prenda");
-			System.out.println("0: salir");
-			System.out.println("1: saco");
-			System.out.println("2: pantalon");
-			System.out.println("3: camisa");
+			System.out.println("1 = Saco");
+			System.out.println("2 = Pantalon");
+			System.out.println("3 = Camisa");
+			System.out.println("4 = Volver atrás");
 			input = s.nextLine();
 			n2 = Integer.parseInt(input);
 			Prenda prenda;
 			switch (n2) {
-			case 0:
-				flag2 = false;
-				break loop2;
 			case 1:
 				prenda = new Saco(false);
 				break;
@@ -76,6 +70,9 @@ public class Main {
 			case 3:
 				prenda = new Camisa(false);
 				break;
+			case 4:
+				flag2 = false;
+				break loop2;
 			default:
 				System.out.println("ERROR: Prenda inexistente.");
 				break loop2;
@@ -96,18 +93,19 @@ public class Main {
 			System.out.print("Desea registrar venta? S/N: ");
 			input = s.nextLine();
 			if (input.contentEquals("s")) {
-				System.out.print("\nCantidad?: ");
+				System.out.print("Cantidad: ");
 				input = s.nextLine();
 				Ventas venta = new Ventas(prenda, Integer.parseInt(input), valorFijo);
 				venta.mostrarVenta(venta);
 				Arrayventas.add(venta);
 			}
 		}
-	}/*
-	private static void mostrarVentas(){
-	Double totalVentasDelDia=(double) 0;
-	ArrayList<Double>precios=new ArrayList<Double>();
-	System.out.println("Entro: "+Arrayventas.size());
+	}
+	public static double calcularVentas(ArrayList<Ventas> Arrayventas){	// DEVUELVE EL TOTAL DE VENTAS DEL DIA
+	//System.out.println("Entro: "+Arrayventas.size());      tamaño del array
 	Arrayventas.forEach(v->v.mostrarVenta(v));		//muestra cada venta realizada
-	}			TODAVIA NO FUNCIONA ESTA MIERDA, NO PUEDO SACAR LA SUMATORIA ¬¬ 	*/
+	double sumDia = Arrayventas.stream().mapToDouble(ventas->ventas.calcularPrecio()).sum(); //mapea y obtengo un Stream con todos los precios y el sum calcula el total
+	System.out.println("Ganancia del dia: $"+sumDia+"\n");
+	return sumDia;
+	}	
 }
---
+++ src/paquete/TestVentas.java
@@ -0,0 +1,36 @@
+package paquete;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestVentas {
+	ArrayList<Ventas> arrayVentas=new ArrayList<Ventas>();
+	static int valorFijo=100;
+	Prenda sacoNacional=new Saco(false);	//$300
+	Prenda camisaImportada=new Camisa(true);  //$200 
+	Prenda pantalonNacional=new Pantalon(false); //$250
+	static double total=0;
+	@Test
+	public void testPrecioSacoNacional() {
+		Assert.assertEquals(400, sacoNacional.precioFinal(valorFijo, sacoNacional),0); //300 + el valorFijo(100)
+	}
+	@Test
+	public void testPrecioCamisaImportada(){
+		Assert.assertEquals(390, camisaImportada.precioFinal(valorFijo, camisaImportada),0); //(200+100)*1,3
+	}
+	@Test
+	public void testVentas(){
+		Ventas ventaSaco = new Ventas(sacoNacional, 3, valorFijo); //3 sacos=$1200
+		Ventas ventaPantalon = new Ventas(pantalonNacional,2,valorFijo); //2 pantalones=$700
+		Ventas ventaCamisa = new Ventas(camisaImportada,2,valorFijo); //2 camisas=$780
+		arrayVentas.add(ventaSaco);
+		arrayVentas.add(ventaPantalon);
+		arrayVentas.add(ventaCamisa);
+		double total=Main.calcularVentas(arrayVentas);
+		Assert.assertEquals(2680, total,0);
+	}	
+}
